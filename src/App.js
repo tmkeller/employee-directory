4a1251818f3f20fdf2a29948b05680c71080cc0b
Tim Keller <timothy.m.keller@gmail.com>
2021-02-17 01:07:32 -0800

    Added wrapper and employee cards to main page. Added API script for Axios and imported it into the index for Wrapper component. Updated JSON packages with Axios.

--- src/App.js
+++ src/App.js
@@ -38,22 +38,15 @@ class App extends React.Component {
 
   render() {  
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Title>Employees</Title>
+        { this.state.employees.map( elem => <EmployeeCard name={ elem.name }
+          image={ elem.image }
+          occupation={ elem.occupation }
+          location={ elem.location } 
+          id={ elem.id }
+          delete={ this.deleteFunction } />)}
+      </Wrapper>
     );
   }
 }
